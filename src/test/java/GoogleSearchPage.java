333a55a41166dd6c9ad06fe7d66ada991e5fda77
MakovskyiNikolai <MakovskyiNikolai@users.noreply.github.com>
2019-06-03 16:58:17 +0300

    Initial commit - NEW PROJECT

---
+++ src/test/java/GoogleSearchPage.java
@@ -0,0 +1,43 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertEquals;
+
+public class GoogleSearchPage {
+  private WebDriver driver;
+
+  @Before
+  public void setUp(){
+    System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+    driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get("http://www.google.com");
+  }
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+  }
+
+
+  @Test
+  public void testGoogleSearch(){
+    WebElement element = driver.findElement(By.name("q"));
+    element.sendKeys("Selenium testing tools cookbook");
+    element.submit();
+    new WebDriverWait(driver,10).until(new ExpectedCondition<Boolean>() {
+public Boolean apply(WebDriver d){
+  return d.getTitle().toLowerCase().startsWith("selenium testing tools cookbook");
+  }
+    });
+assertEquals("Selenium testing tools cookbook - Пошук Google",
+        driver.getTitle());
+    }
+
+}
